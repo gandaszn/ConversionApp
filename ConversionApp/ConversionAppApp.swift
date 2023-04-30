c994ec0e7d4fdfb138b167f7eb9bb64b639c9bbd
Ganda <gandaszn@gmail.com>
2023-04-30 15:01:01 +0700

    Initial Commit

---
+++ ConversionApp/ConversionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionAppApp.swift
+//  ConversionApp
+//
+//  Created by Josua Hutapea on 30/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ConversionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
